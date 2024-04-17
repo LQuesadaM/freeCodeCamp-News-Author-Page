efddb97e3df9f31dd4d27da3e66422a7f387bf82
LQuesadaM <luisquesada163@gmail.com>
2024-04-17 17:07:08 -0300

    👽️ Fetch.. getting the freecodecamp API
    getting the freecodecamp API using fetch API method

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const authorContainer = document.getElementById("author-container");
 const loadMoreBtn = document.getElementById("load-more-btn");
+
+/* The Fetch API is a built-in JavaScript interface to make network requests to a server. It has a fetch() method you can use to make GET, POST, PUT, or PATCH requests. In this project, you'll make a GET request to a URL for a JSON file with information about authors on freeCodeCamp News. */
+
+fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.error(`There was an error: ${err}`));
