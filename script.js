9fb80612d3a68c8f962c45ea7f2836e3ef0f4cf1
LQuesadaM <luisquesada163@gmail.com>
2024-04-17 18:50:12 -0300

    🍻 Added  startingIndex, endingIndex, authorDataArr
    let variables startingIndex, endingIndex, with numeric values 0 and 8. In addition, I added authorDataArr as an empty array

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const authorContainer = document.getElementById("author-container");
 const loadMoreBtn = document.getElementById("load-more-btn");
 
+let startingIndex = 0;
+let endingIndex = 8;
+let authorDataArr = [];
+
 /* The Fetch API is a built-in JavaScript interface to make network requests to a server. It has a fetch() method you can use to make GET, POST, PUT, or PATCH requests. In this project, you'll make a GET request to a URL for a JSON file with information about authors on freeCodeCamp News. */
 
 fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
