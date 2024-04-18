ceb228ff4fe6962f5497f99fc3463bafe5eb48fd
LQuesadaM <luisquesada163@gmail.com>
2024-04-18 07:58:23 -0300

    ✨ Make the Load More Authors button fetch more authors
    incrementing the startingIndex and endingIndex variables to 8.ddeadded a click event to the loadMoreBtn button to call the fetchMoreAuthors function

--- script.js
+++ script.js
@@ -15,7 +15,12 @@ fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
   })
   .catch((err) => console.error(`There was an error: ${err}`));
 
-  //* More authors
+//* More authors
+const fetchMoreAuthors = () => {
+  startingIndex += 8;
+  endingIndex += 8;
+  displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+};
 
 const displayAuthors = (authors) => {
   authors.forEach(({ author, image, url, bio }, index) => {
@@ -28,3 +33,5 @@ const displayAuthors = (authors) => {
     </div>`;
   });
 };
+
+loadMoreBtn.addEventListener("click", fetchMoreAuthors);
